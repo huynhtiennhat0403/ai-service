eb655775f99184fa0c0bea1032ee55fdcd2f6dfc
huynhtiennhat0403 <huynhtiennhat0403@gmail.com>
2026-03-05 15:52:04 +0700

    feat(inference): setup AI utility functions for model loading and feature extraction

---
+++ inference.py
@@ -0,0 +1,37 @@
+import cv2
+import torch
+import numpy as np
+from torchvision import transforms
+
+# Import model từ thư mục 
+from models.mobilefacenet import MobileFaceNet
+from models.face_detector import FaceDetector
+
+def load_model(weights_path, device='cpu'):
+    print("Đang nạp bộ não AI...")
+    model = MobileFaceNet(embedding_size=128).to(device)
+    model.load_state_dict(torch.load(weights_path, map_location=device, weights_only=True))
+    model.eval()
+    return model
+
+def preprocess_face(face_img):
+    """Tiền xử lý khuôn mặt đã được cắt từ Webcam."""
+    face_img = cv2.cvtColor(face_img, cv2.COLOR_BGR2RGB)
+    transform = transforms.Compose([
+        transforms.ToPILImage(),
+        transforms.Resize((112, 112)),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])
+    ])
+    return transform(face_img).unsqueeze(0)
+
+def get_embedding(model, img_tensor, device='cpu'):
+    """Trích xuất vector 128 chiều."""
+    img_tensor = img_tensor.to(device)
+    with torch.no_grad():
+        feature = model(img_tensor)
+    return feature.cpu().numpy().flatten()
+
+def cosine_similarity(vec1, vec2):
+    return np.dot(vec1, vec2) / (np.linalg.norm(vec1) * np.linalg.norm(vec2))
+
